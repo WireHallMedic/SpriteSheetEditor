7687dbba3f705f2b4a4d6f0c6f17ac98afdcaf48
Michael <MichaelWidler@yahoo.com>
2024-06-09 08:31:05 -0700

    Show tile on canvas.

--- SpriteSheetEditor/CanvasPanel.java
+++ SpriteSheetEditor/CanvasPanel.java
@@ -19,9 +19,9 @@ public class CanvasPanel extends JPanel
       super.paint(g);
       Graphics2D g2d = (Graphics2D)g;
       
-      if(SSEEngine.getFullImage() != null)
+      if(SSEEngine.getCurTile() != null)
       {
-         g2d.drawImage(SSEEngine.getFullImage(), 0, 0, null);
+         g2d.drawImage(SSEEngine.getCurTile(), 0, 0, null);
       }
    }
 }
