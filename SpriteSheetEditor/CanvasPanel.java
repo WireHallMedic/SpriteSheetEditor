d6b51c16b4baaaeb3591411d5edfa0ec6f209a2e
Michael <MichaelWidler@yahoo.com>
2024-06-09 08:41:21 -0700

    Dymanically resize tile.

--- SpriteSheetEditor/CanvasPanel.java
+++ SpriteSheetEditor/CanvasPanel.java
@@ -2,10 +2,12 @@ package SpriteSheetEditor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.*;
 
 public class CanvasPanel extends JPanel
 {
    private SSEMain parent;
+   private int sizeMultiplier = 1;
    
    public CanvasPanel(SSEMain p)
    {
@@ -18,10 +20,19 @@ public class CanvasPanel extends JPanel
    {
       super.paint(g);
       Graphics2D g2d = (Graphics2D)g;
+      BufferedImage curTile = SSEEngine.getCurTile();
       
-      if(SSEEngine.getCurTile() != null)
+      if(curTile != null)
       {
-         g2d.drawImage(SSEEngine.getCurTile(), 0, 0, null);
+         calcSizeMultiplier(curTile);
+         g2d.drawImage(curTile, 0, 0, curTile.getWidth() * sizeMultiplier, curTile.getHeight() * sizeMultiplier, null);
       }
+   }
+   
+   public void calcSizeMultiplier(BufferedImage img)
+   {
+      int maxMultWide = this.getWidth() / img.getWidth();
+      int maxMultHigh = this.getHeight() / img.getHeight();
+      sizeMultiplier = Math.min(maxMultWide, maxMultHigh);
    }
 }
