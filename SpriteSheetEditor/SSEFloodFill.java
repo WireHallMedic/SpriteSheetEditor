bc4164c5c043578b34be62268ab7f83836fa7a70
Michael <MichaelWidler@yahoo.com>
2025-01-03 16:34:11 -0800

    Implement flood fill.

--- SpriteSheetEditor/SSEFloodFill.java
+++ SpriteSheetEditor/SSEFloodFill.java
@@ -1,7 +1,72 @@
 package SpriteSheetEditor;
 
+import java.awt.image.*;
+import java.util.*;
+import java.awt.*;
 
 public class SSEFloodFill
 {
-
+   private int startX;
+   private int startY;
+   private int targetColor;
+   private BufferedImage img;
+   private boolean[][] closedMap;
+   private Vector<int[]> locList;
+   
+   
+   public SSEFloodFill(BufferedImage image, int x, int y)
+   {
+      startX = x;
+      startY = y;
+      img = image;
+      closedMap = new boolean[image.getWidth()][image.getHeight()];
+      locList = new Vector<int[]>();
+      start();
+   }
+   
+   private void start()
+   {
+      targetColor = img.getRGB(startX, startY);
+      int[] origin = {startX, startY};
+      locList.add(origin);
+      process(startX, startY);
+   }
+   
+   private void process(int x, int y)
+   {
+      if(isInBounds(x, y))
+      {
+         closedMap[x][y] = true;
+         // match, add to list and process neighbors
+         if(img.getRGB(x, y) == targetColor)
+         {
+            int[] listItem = {x, y};
+            locList.add(listItem);
+            if(isValidNeighbor(x + 1, y)) process(x + 1, y);
+            if(isValidNeighbor(x - 1, y)) process(x - 1, y);
+            if(isValidNeighbor(x, y + 1)) process(x, y + 1);
+            if(isValidNeighbor(x, y - 1)) process(x, y - 1);
+         }
+      }
+   }
+   
+   private boolean isInBounds(int x, int y)
+   {
+      return x >= 0 &&
+             y >= 0 &&
+             x < img.getWidth() &&
+             y < img.getHeight();
+   }
+   
+   private boolean isValidNeighbor(int x, int y)
+   {
+      return isInBounds(x, y) && !closedMap[x][y];
+   }
+   
+   public void applyFill()
+   {
+      Color c = new Color(targetColor);
+      for(int i = 0; i < locList.size(); i++)
+         SSEEngine.writeColor(locList.elementAt(i)[0], locList.elementAt(i)[1], c);
+   }
 }
